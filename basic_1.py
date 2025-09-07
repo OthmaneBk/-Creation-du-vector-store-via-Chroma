bcc9f237ac7e358c48696284deb03446be839d07
Othmane Kella Bennani <163937940+OthmaneBk@users.noreply.github.com>
2025-09-07 13:58:14 +0200

    Update basic_1.py

--- basic_1.py
+++ basic_1.py
@@ -8,7 +8,7 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 
 # --- IMPORTANT CHANGE HERE: Set HF_HOME environment variable ---
 # Define the path for the Hugging Face cache directory
-hf_cache_directory = "D:\\KELLA BENNANI OTHMANE\\Othmane\\models"
+hf_cache_directory = "************"
 #os.makedirs(hf_cache_directory, exist_ok=True)
 os.environ['HF_HOME'] = hf_cache_directory
 print(f"Hugging Face cache directory set to: {os.environ['HF_HOME']}")
@@ -35,27 +35,19 @@ if not os.path.exists(persistent_directory):
     documents = loader.load()
 
     # Split the document into chunks
-    #text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)#chaque chunk a 1000 caractères et chunk_overlap=0 ceci permet que chaque chunk est independant pas de répetitions des mots
+    #text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     tokens_splitter = TokenTextSplitter(chunk_size=240, chunk_overlap=20)
-    """
-        Chunk 1 : ABCDE
-        Chunk 2 : FGHIJ
-        
-        avec overlap = 2 
-            Chunk 1 : ABCDE
-            Chunk 2 : CDEFG -> répétition de qlq letteres dans les deux chunks
-    """ 
+
     docs = tokens_splitter.split_documents(documents)
 
     # Create embeddings
     print("\n--- Creating embeddings ---")
     # Initialize the SentenceTransformer model
-    #By default, input text longer than 256 word pieces is truncated. (supprimés)
     model_name = 'sentence-transformers/all-MiniLM-L6-v2'
     sentence_transformer_model = SentenceTransformer(model_name)
 
     # --- WRAP THE SentenceTransformer MODEL WITH HuggingFaceEmbeddings ---
-    embeddings = HuggingFaceEmbeddings(client=sentence_transformer_model) # Use 'client' for an already loaded model
+    embeddings = HuggingFaceEmbeddings(client=sentence_transformer_model) 
     print("\n--- Finished creating embeddings ---")
 
     # Create the vector store and persist it automatically
